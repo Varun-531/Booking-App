d0ebd3d4cfd749ce3f9522f51ce5308022bedd8d
ChVarunFE <chennaparapu.varun@razorpay.com>
2025-03-25 18:56:46 +0530

    Main

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	
+	fmt.Println("Welcome to Our", conferenceName ,"Booking App")
+	fmt.Println("Book Your tickets here")
+}
