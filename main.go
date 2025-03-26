c8e48abdad68074e9dc808c758bcb41ec962a378
ChVarunFE <chennaparapu.varun@razorpay.com>
2025-03-26 10:15:16 +0530

    data types

--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-	
-	fmt.Println("Welcome to Our", conferenceName ,"Booking App")
-	fmt.Println("Book Your tickets here")
+	var remainingTickets = 50
+
+	fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
+
+	fmt.Printf("Welcome to Our %v Booking App\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v tickets are remaining\n",conferenceTickets, remainingTickets)
+	fmt.Println("Book Your tickets Now!!")
+
+	var userName string
+	userName = "John Doe"
+	fmt.Println(userName)
 }
